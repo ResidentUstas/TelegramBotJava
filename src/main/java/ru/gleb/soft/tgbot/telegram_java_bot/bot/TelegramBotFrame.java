353466c19d3a0444ad0c9b8dd03a0e6139c204eb
g.yakovlev <yakovleffgleb@yandex.ru>
2025-01-27 11:10:33 +0300

    chg - add Pasha Bot

--- src/main/java/ru/gleb/soft/tgbot/telegram_java_bot/bot/TelegramBotFrame.java
+++ src/main/java/ru/gleb/soft/tgbot/telegram_java_bot/bot/TelegramBotFrame.java
@@ -11,107 +11,107 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import ru.gleb.soft.tgbot.telegram_java_bot.domain.entities.Client;
-import ru.gleb.soft.tgbot.telegram_java_bot.domain.services.ClientService;
-import ru.gleb.soft.tgbot.telegram_java_bot.domain.services.DishService;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Component
 public class TelegramBotFrame extends TelegramLongPollingBot {
     @Autowired
     DefaultBotOptions defaultBotOptions;
-    private final ClientService clientService;
-    private final DishService dishService;
+    private List<String> phrases;
+    private int phrases_count;
+    private Random rand;
+    private int mode;
 
-    public TelegramBotFrame(DefaultBotOptions defaultBotOptions, @Value("${bot.token}") String botToken, ClientService clientService, DishService dishService) {
+    public TelegramBotFrame(DefaultBotOptions defaultBotOptions, @Value("${bot.token}") String botToken) {
         super(defaultBotOptions, botToken);
-        this.clientService = clientService;
-        this.dishService = dishService;
+
         List<BotCommand> botCommandList = new ArrayList<>();
-        botCommandList.add(new BotCommand("/getMenu", "Посмотреть меню на сегодня"));
+        phrases = getPhrasesList();
+        phrases_count = phrases.size();
+        rand = new Random();
+        mode = 0;
         try {
-             this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
-        }catch (TelegramApiException e){
+            this.execute(new SetMyCommands(botCommandList, new BotCommandScopeDefault(), null));
+        } catch (TelegramApiException e) {
         }
     }
 
     @Override
     public void onUpdateReceived(Update update) {
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String messageText = update.getMessage().getText();
-            long chatId = update.getMessage().getChatId();
-            String name = update.getMessage().getChat().getFirstName();
-            switch (messageText) {
-                case "/start":
-                    startCommandReceived(chatId, update.getMessage().getChat().getFirstName());
-                    Client client = new Client();
-                    client.setLogin(update.getMessage().getChat().getFirstName());
-                    client.setName(update.getMessage().getChat().getLastName());
-                    client.setChatId(chatId);
-                    clientService.save(client);
-                    break;
-                case "/getmenu":
-                    String answer = "Привет, " + name + ". Актуальное меню: \r\n";
-                    sendMessage(chatId, answer);
-                    getMenuReceived(chatId);
-                    break;
+            if (update.getMessage().getText().equals("/start")) {
+                sendMessage(update.getMessage().getChatId(), "Привет петухи", 0);
             }
-        } else if (update.hasCallbackQuery()) {
-            var chatId = update.getCallbackQuery().getMessage().getChatId();
-            var userId = update.getCallbackQuery().getFrom().getId();
-            var userName = update.getCallbackQuery().getFrom().getFirstName();
-            var receivedMessage = update.getCallbackQuery().getData();
-
-            botAnswerUtils(receivedMessage, chatId, userName);
-        }
-    }
-
-    private void botAnswerUtils(String receivedMessage, long chatId, String userName) {
-        switch (receivedMessage) {
-            case "/makeOrder":
-                String answer = "Делаем заказ";
-                sendMessage(chatId, answer);
-                break;
-            default:
-                break;
-        }
-    }
-
-    private void startCommandReceived(Long chatId, String name) {
-        String answer = "Привет, " + name + ". Булушка мастер гастрономии, лучший булочный шеф. Готовы сделать закаааз?";
-        sendMessage(chatId, answer);
-    }
-
-    private void getMenuReceived(Long chatId) {
-        String answer = "";
-        var menu = dishService.findAll();
-        for (var dish : menu){
-            answer += "Блюдо: " + dish.getName() + " \r\n"
-                    + "Состав: " + dish.getStruct() + " \r\n"
-                    + "Время приготовления: " + dish.getCookTime();
-            SendMessage sendMessage = new SendMessage();
-            sendMessage.setChatId(String.valueOf(chatId));
-            sendMessage.setText(answer);
-            sendMessage.setReplyMarkup(Buttons.inlineMarkup());
-            try {
-                execute(sendMessage);
-            } catch (TelegramApiException e) {
-
+            if (update.getMessage().getText().equals("/add")) {
+                mode = 1;
+                sendMessage(update.getMessage().getChatId(), "Пришлите фразы для добавление", 0);
             }
-            answer = "";
+            if (update.getMessage().getText().equals("/stopadd")) {
+                mode = 0;
+                sendMessage(update.getMessage().getChatId(), "Фразы добавлены", 0);
+            }
         }
+
+        if (update.hasMessage() && update.getMessage().getReplyToMessage() != null)
+            if (mode == 0) {
+                if (update.getMessage().getReplyToMessage().getFrom().getId() == 8013072863L) {
+                    var chatId = update.getMessage().getChatId();
+                    var replyMess = update.getMessage();
+                    String messageText = phrases.get(rand.nextInt(phrases_count));
+                    sendMessage(chatId, messageText, replyMess.getMessageId());
+                }
+            } else {
+                var newPhrase = update.getMessage();
+                try {
+                    addPhrase(newPhrase.getText());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
     }
-    private void sendMessage(Long chatId, String textToSend) {
+
+    private void sendMessage(Long chatId, String textToSend, int replyId) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(String.valueOf(chatId));
+        if (replyId > 0) {
+            sendMessage.setReplyToMessageId(replyId);
+        }
         sendMessage.setText(textToSend);
         try {
             execute(sendMessage);
         } catch (TelegramApiException e) {
 
         }
+    }
+
+    private ArrayList<String> getPhrasesList() {
+        try {
+            var reader = new BufferedReader(new FileReader("src\\main\\resources\\phrases.txt"));
+            String line = reader.readLine();
+            var result = new ArrayList<String>();
+            result.add(line);
+            while (line != null) {
+                line = reader.readLine();
+                result.add(line);
+            }
+
+            reader.close();
+            return result;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private void addPhrase(String phrase) throws IOException {
+        FileWriter writer = new FileWriter("src\\main\\resources\\phrases.txt", true);
+        BufferedWriter bufferWriter = new BufferedWriter(writer);
+        bufferWriter.write(phrase + "\r\n");
+        bufferWriter.close();
     }
 
     @Override
@@ -121,6 +121,6 @@ public class TelegramBotFrame extends TelegramLongPollingBot {
 
     @Override
     public String getBotUsername() {
-        return "bot_eat_maker";
+        return "Paha_Svin";
     }
 }
