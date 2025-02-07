602f1d4abcdd6476e44ca7a7cf511481deafa018
g.yakovlev <yakovleffgleb@yandex.ru>
2025-02-07 10:40:54 +0300

    chg - add webhooks path and phrases set schediler add ping

--- src/main/java/ru/gleb/soft/tgbot/telegram_java_bot/controller/WebhookController.java
+++ src/main/java/ru/gleb/soft/tgbot/telegram_java_bot/controller/WebhookController.java
@@ -2,13 +2,13 @@ package ru.gleb.soft.tgbot.telegram_java_bot.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import ru.gleb.soft.tgbot.telegram_java_bot.bot.WebhookBot;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Slf4j
 @RestController
@@ -16,6 +16,14 @@ import ru.gleb.soft.tgbot.telegram_java_bot.bot.WebhookBot;
 @RequestMapping("/bot")
 public class WebhookController {
     private final WebhookBot telegramBot;
+
+    @GetMapping("/ping")
+    public String ping() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+        log.info("отвечаю на пинг");
+        return dtf.format(now);
+    }
 
     @RequestMapping(value = "/update", method = RequestMethod.POST)
     public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
